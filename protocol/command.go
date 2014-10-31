89806ac3ea014aca0faa1671c694c45aad0f1d78
latermoon <lptmoon@gmail.com>
2014-11-01 02:06:11 +0800

    coding...

--- protocol/command.go
+++ protocol/command.go
@@ -3,13 +3,62 @@ package protocol
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
 )
 
 // Redis Command
 type Command [][]byte
 
+func NewCommand(args ...[]byte) Command {
+	return Command(args)
+}
+
 func (c Command) Len() int {
 	return len(c)
+}
+
+func (c Command) ArgAtIndex(i int) (arg []byte, err error) {
+	if i >= c.Len() {
+		return nil, errors.New(fmt.Sprintf("index out of range %d/%d", i, c.Len()))
+	} else {
+		return c[i], nil
+	}
+}
+
+func (c Command) StringAtIndex(i int) (string, error) {
+	if arg, err := c.ArgAtIndex(i); err == nil {
+		return string(arg), nil
+	} else {
+		return "", err
+	}
+}
+
+func (c Command) IntAtIndex(i int) (n int, err error) {
+	if f, err := c.FloatAtIndex(i); err == nil {
+		return int(f), nil
+	} else {
+		return 0, nil
+	}
+}
+
+func (c Command) Int64AtIndex(i int) (int64, error) {
+	if f, err := c.FloatAtIndex(i); err == nil {
+		return int64(f), nil
+	} else {
+		return 0, err
+	}
+}
+
+func (c Command) FloatAtIndex(i int) (float64, error) {
+	if arg, err := c.StringAtIndex(i); err != nil {
+		return 0, err
+	} else if n, err := strconv.ParseFloat(arg, 64); err != nil {
+		return 0, err
+	} else {
+		return n, nil
+	}
 }
 
 func (c Command) Bytes() []byte {
